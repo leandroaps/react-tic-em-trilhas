76a722f9b5f440146b6d9460b65f0382b17a60bf
Leandro A. Siqueira <leandro.siqueira@venturus.org.br>
2025-11-04 08:44:53 -0300

    base project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { IconInnerShadowTop } from '@tabler/icons-react';
+
+function App() {
+  return (
+    <header className="flex h-16 shrink-0 items-center gap-2 border-b">
+      <div className="flex items-center gap-2 px-3">
+        <a href="#">
+          <IconInnerShadowTop className="!size-5" />
+          <span className="text-base font-semibold">Acme Inc.</span>
+        </a>
+      </div>
+    </header>
+  );
+}
+
+export default App;
